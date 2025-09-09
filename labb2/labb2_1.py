5d085e943480b7d69503ad9ae93b05eee44ba37e
ibbe479 <ibrahim.halawani04@gmail.com>
2025-09-09 02:39:22 +0200

    Update labb2_1.py

--- labb2/labb2_1.py
+++ labb2/labb2_1.py
@@ -1,3 +1,5 @@
+##############################      upg 1      ################################################
+
 class dog :
     def __init__(self, name, breed, age, asleep, color):
         self.name = name
@@ -9,28 +11,104 @@ class dog :
     def __str__(self):
         return(f"namn: {self.name} ras: {self.breed} ålder: {self.age} sover hunden {self.asleep} färg: {self.color}")
     
-    def se_info():
+    def se_info(self):
         """this will print out info"""
-        pass
+        return f"mitt namn är {self.name} och jag är {self.age} år gammal"
 
-    def is_dog_asleep():
+    def is_dog_asleep(self):
         """will chek if the dog is asleep with true or false"""
-        pass
+        if self.asleep == True:
+            #if dog is sleeping
+            return f"{self.name} sover just nu"
+        else:
+            #if dog is not sleeping
+            return f"{self.name} är vaken"
 
-    def wake_up_dog():
+    def wake_up_dog(self):
         """if dog is asleep(true) then make it false"""
-        pass
+        if self.asleep==True:
+            #if dog is sleping
+            self.asleep=False
+            return f"nu är {self.name} vaken"
+        else:
+            #id dog is not sleeping
+            return f"{self.name} är redan vaknen"
 
-    def make_dog_go_to_sleep():
+
+
+    def make_dog_go_to_sleep(self):
         """if dog is awake(false) then make it true"""
-        pass
+        if self.asleep!=True:
+            #if dog is sleping
+            self.asleep=True
+            return f"nu har {self.name} sommant"
+        else:
+            #id dog is not sleeping
+            return f"{self.name} sover redan"
 
-    def show_fur_and_color():
+
+    def show_fur_and_color(self):
         """show waht color the dog have"""
-        pass
+        return f"färgena på {self.name} är {self.color} "
+
+Doug= dog("Doug", "Pug", 8, True, ["black", "white", "beige"])
 
 
-Doug= dog ("Doug", "pug", 8, False,"black, white and beige"  )
+print(Doug.se_info())
+print(Doug.is_dog_asleep())
+print(Doug.wake_up_dog())
+print(Doug.make_dog_go_to_sleep())
+print(Doug.show_fur_and_color())
 
-print(Doug)
+hund={
+    "namn": "Doug",
+    "ras":"pug",
+    "ålder": 8,
+    "sover": False,
+    "färg":["svart", "vit", "beige"]
+}
+ 
+def print_info():
+    """här kommer hundens info att skrivas """    
+    print (f"woof! i'm {hund['namn']} the {hund['ras']} ({hund['ålder']} år)") 
 
+def is_dog_asleep1():
+    """kollar om hunder sover eller inte"""
+    if hund["sover"]==True:
+        #om hunden sover
+        print(f"{hund['namn']} sover")
+    else:
+        #om hunder är vaken
+        print(f"{hund['namn']} är vaken")
+
+def wake_up_dog1():
+    """kollar om hunder sover och får den att vakna"""
+    if hund["sover"]==True:
+        #om hunden sover 
+        hund["sover"]=False
+        print(f"{hund['namn']} är vaken nu")
+    else:
+        #om hunden redan är vakne
+        print(f"{hund['namn']} är redan vaken")
+
+def make_dog_go_to_sleep1():
+    """kollar om hunden är vaken och får den att somna """
+    if hund["sover"]!=True:
+        #om hunden är vaken 
+        hund["sover"]=True
+        print(f"{hund['namn']} har somnat")
+    else:
+        #om hunden redan är vakne
+        print(f"{hund['namn']} sover redan")
+
+def show_fur_and_color1():
+    """visar vilka fägrer hunden har"""
+    print(f"{hund['namn']} har färgerna {hund['färg']}")
+
+        
+#print_info()
+#is_dog_asleep1()
+#wake_up_dog1()
+#make_dog_go_to_sleep1()
+#show_fur_and_color1()
+##############################      upg 1      ################################################

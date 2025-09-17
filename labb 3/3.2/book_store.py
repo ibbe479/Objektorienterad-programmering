43ff6743bfdcf474668c97007027b08b57f22f46
ibbe479 <ibrahim.halawani04@gmail.com>
2025-09-17 22:18:33 +0200

    Update book_store.py

---
+++ labb 3/3.2/book_store.py
@@ -0,0 +1,56 @@
+from book_inventory import Book_inventory
+
+class shop: 
+    def __init__(self):
+        self.collection = inventory()
+
+    def show_menu(self):
+        print("\nMenu:")
+        print("1. Add product")
+        print("2. Show products")
+        print("3. Remove product")
+        print("4. Update price")
+        print("5. Exit")
+
+    def run(self):
+        while True:
+            self.show_menu()
+            val= input("vad vill du göra? ")
+            
+
+            if val=="1":
+                print("lägg till en produkt")
+                prod_nr=input("vad är produkt nummret? ")
+                namnet= input("vad heter produkten? ")
+                priset= input ("vad kostar varan? ")
+                self.collection.add_product(prod_nr, namnet, priset)
+
+            elif val == "2":
+                print("visar produkterna")
+                self.collection.show_all()
+
+            elif val == "3":
+                print("raderar en produkt")
+                art_nummer=input("skriv in artikelnummret du vill radera: ")
+                self.collection.delete(art_nummer)
+                
+            elif val == "4":
+                print("print uppdatera priset")
+                prod_nr=input("vad är produckt nummret: ")
+                nya_priset=input("vad är det nya priset du vill lägga in ")
+                self.collection.uppdate_price(prod_nr, nya_priset)
+            elif val == "5":
+                print("hejdå")
+                break
+
+            else:
+                print("oglitigt val försök igen")
+
+
+
+if __name__ == "__main__":
+    program = shop()
+    program.run()
+
+
+#https://drive.google.com/file/d/1Ax5CR2i-8XqcKOj8ziyVi_0k-F7as7Tj/view?usp=sharing
